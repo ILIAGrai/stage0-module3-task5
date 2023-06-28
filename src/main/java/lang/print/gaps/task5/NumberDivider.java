8f7469af5414ec69db4adb939fc814ab77c1a63e
Ilucha <pozta_01@mail.ru>
2023-06-28 14:03:24 +0300

    Num4

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,9 +1,9 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(double toBeDivided) {
+    void divide(int toBeDivided) {
 
-        double Raz =toBeDivided/5;
+        float Raz =toBeDivided/5f;
         System.out.println(Raz);
     }
 }
